0ce00fdc3731c09e24f67e3cabc2177eda06109a
eonsubae <eonsubae@naver.com>
2019-10-04 03:23:51 +0900

    Add page(wordbook)

---
+++ pages/wordbook/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BaseLayout from '../../components/_App/BaseLayout';
+
+const Wordbook: React.FC = () => {
+  return (
+    <BaseLayout>
+      <h1>Wordbook</h1>
+    </BaseLayout>
+  );
+};
+
+export default Wordbook;

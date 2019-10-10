4e67d837823884c1d41dbea83d9c2cec391b8163
eonsubae <eonsubae@naver.com>
2019-10-11 02:46:49 +0900

    Add Logout component

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+import cookie from 'js-cookie';
+
+import '../styles/components/auth/logout.scss';
+
 const Logout = () => {
+  
+  useEffect(() => {
+    const token = cookie.get('token');
+    if (token) {
+      cookie.remove('token');
+      Router.push('/');
+    } else {
+      Router.push('/login');
+    }
+  }, []);
+
   return (
-    <div>Logout</div>
+    <div className="logout-message">
+      Please. Waiting for a second. Logout in progress
+    </div>
   );
 };
 

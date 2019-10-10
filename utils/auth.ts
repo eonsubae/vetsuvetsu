5891d42171aa369ec0f1a276adbdfb81875f1303
eonsubae <eonsubae@naver.com>
2019-10-11 00:19:08 +0900

    Update Signup that can validate between password and confirm password

--- utils/auth.ts
+++ utils/auth.ts
@@ -5,3 +5,12 @@ export function handleLogin(token) {
   cookie.set('token', token);
   Router.push('/login');
 };
+
+export function validatePassword(pw, confirmPw) {
+  if (pw === confirmPw) {
+    return true;
+  }
+
+  console.log('Error : password and confirm password is not matched');
+  return false;
+}
